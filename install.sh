7c9c496cf8788cde2076058fce6c89d02c572762
maiquelb <maiquelb@gmail.com>
2023-03-02 15:37:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ cd catkin_wsp
 wget https://github.com/embedded-mas/embedded_mas_ros_example_package/raw/master/src.zip
 unzip src.zip
 catkin_make
-source catkin_wsp/devel/setup.bash
+source devel/setup.bash
 rosrun embedded_mas_examples sum_array_server.py
