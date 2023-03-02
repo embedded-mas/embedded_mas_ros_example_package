cd1463098effe66c4303d2567dce692a9ce1dcd4
maiquelb <maiquelb@gmail.com>
2023-03-01 23:22:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@ apt install wget
 apt install unzip
 mkdir catkin_wsp
 cd catkin_wsp
-#mkdir src
-catkin_make
 wget https://github.com/embedded-mas/embedded_mas_ros_example_package/raw/master/src.zip
 unzip src.zip
 catkin_make
