d721183b0e8b971829d596c9f2eebdf89d2d09d6
maiquelb <maiquelb@gmail.com>
2023-03-06 17:00:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,12 @@ sudo apt install -y unzip
 sudo apt install -y make
 sudo apt install -y build-essential g++
 mkdir catkin_wsp
-cd catkin_wsp
 wget https://github.com/embedded-mas/embedded_mas_ros_example_package/raw/master/src.zip
-unzip src.zip
+unzip src.zip -d catkin_wsp
+cd catkin_wsp
 catkin_make
-source devel/setup.bash
+cd ..
+source catkin_wsp/devel/setup.bash
 
 
 # After run this script, ensure that roscore is running and start the example services with the following command:
