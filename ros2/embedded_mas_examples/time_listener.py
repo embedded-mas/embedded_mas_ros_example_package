f7722d99f46f5128fe49c354e0bbe7c50ae42598
maiquelb <maiquelb@gmail.com>
2023-11-09 14:49:21 -0300

    changes in ros2 time listener

--- ros2/embedded_mas_examples/time_listener.py
+++ ros2/embedded_mas_examples/time_listener.py
@@ -1,41 +1,49 @@
-#!/usr/bin/python3
-
 import rclpy
 from rclpy.node import Node
+
 from std_msgs.msg import Int32
 import time
 
+file_path = 'file.txt' #change to your actual file path
 
-class TimeListener(Node):
-  
-   def __init__(self):
-      super().__init__('time_listener')
-      self.publisher_ = self.create_publisher(Int32,'value1',10)
-      timer_period = 0.1 #seconds
-      self.timer = self.create_timer(timer_period, self.topic_callback)
-      self.i = 0 
-      self.previous_time = None
-   
-   def topic_callback(self):
-     current_time = time.time() * 1000  # Convert time to milliseconds
-     if self.previous_time is not None:
-        time_diff = current_time - self.previous_time
-        self.write_to_file(time_diff)
-     self.previous_time = current_time
+class MinimalSubscriber(Node):
 
-   def write_to_file(self,time_diff):
-      with open('file.txt', 'a') as file:
-         file.write(str(time_diff) + '\n')
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.previous_time = None
+        self.subscription = self.create_subscription(
+            Int32,
+            'value1',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
-def time_listener(args=None):
-   rclpy.init(args=args)
-   rclpy.spin(TimeListener())
+    def listener_callback(self, msg):
+       #/self.get_logger().info('I heard: "%s"' % msg.data)
+       current_time = time.time() * 1000  # Convert time to milliseconds
+       if self.previous_time is not None:
+          time_diff = current_time - self.previous_time
+          self.get_logger().info("Time difference: %.2f ms"+ str(time_diff))
+          self.write_to_file(time_diff)
+       self.previous_time = current_time
+
+    def write_to_file(self,time_diff):
+       with open(file_path, 'a') as file:
+          file.write(str(time_diff)+'\n')
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
-    try:
-        time_listener()
-    except rospy.ROSInterruptException:
-        pass
-
-
-
+    main()
