4a3eac8c32f4f1e1c3a5eb9cb856d2fa8265979e
maiquelb <maiquelb@gmail.com>
2024-04-29 16:55:44 -0300

    unify energy management of turtle 1 and 2 in the same script

--- src/embedded_mas_examples/scripts/paper_jason_ros/energy_turtle1.py
+++ src/embedded_mas_examples/scripts/paper_jason_ros/energy_turtle1.py
@@ -11,6 +11,7 @@ import time
 
 #energy = 100
 energy_turtle1 = -1
+energy_turtle2 = -1
 time_to_wait_min = 1
 time_to_wait_max = 5
 energy_decrement_max = 5
@@ -21,9 +22,11 @@ def consume_energy(req):
     global time_to_wait_min
     global energy_decrement_max
     global energy_turtle1
+    global energy_turtle2
     
     # Cria o tópico para publicar mensagens de tipo Int32
     publisher1 = rospy.Publisher('/turtle1/energy', Int32, queue_size=10)
+    publisher2 = rospy.Publisher('/turtle2/energy', Int32, queue_size=10)
     
     while True:
        if(energy_turtle1>0):
@@ -31,6 +34,12 @@ def consume_energy(req):
           print("Current energy 1: ", energy1)
        
           publisher1.publish(energy1)
+          
+       if(energy_turtle2>0):
+          energy1 = int(energy_turtle2 - random.uniform(1,energy_decrement_max))
+          print("Current energy 2: ", energy2)
+       
+          publisher2.publish(energy2)   
        
        time_to_wait = random.uniform(time_to_wait_min, time_to_wait_max)
        rospy.sleep(time_to_wait)        
@@ -46,11 +55,17 @@ def consume_energy_server():
 def callback_turtle1(data):
     global energy_turtle1
     energy_turtle1 = data.data  # Armazena o valor Int32 em uma variável
-    rospy.loginfo(f"Recebido: {energy_turtle1}")
+    rospy.loginfo(f"Recebido energy turtle 1: {energy_turtle1}")
+    
+def callback_turtle2(data):
+    global energy_turtle2
+    energy_turtle2 = data.data  # Armazena o valor Int32 em uma variável
+    rospy.loginfo(f"Recebido energy turtle 2: {energy_turtle2}")    
 
 if __name__ == "__main__":
   
      # Subscreve ao tópico para ler valores Int32
     rospy.Subscriber('/turtle1/energy', Int32, callback_turtle1)
+    rospy.Subscriber('/turtle2/energy', Int32, callback_turtle2)
 
     consume_energy_server()
